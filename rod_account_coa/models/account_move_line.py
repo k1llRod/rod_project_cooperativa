d7683cdbb81f4c4d8a056667c002b63d803b66ba
Rodrigo <kurt.gf@gmail.com>
2024-10-21 10:25:29 -0400

    [FIX]arreglos para verificar

--- rod_account_coa/models/account_move_line.py
+++ rod_account_coa/models/account_move_line.py
@@ -11,3 +11,10 @@ class AccountMoveLine(models.Model):
         for record in self:
             dollar = round(record.env['res.currency'].search([('name', '=', 'USD')], limit=1).inverse_rate,2)
             record.parcial = round(record.debit / dollar,2) if record.debit > 0 else round(record.credit / dollar,2)
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id(self):
+        for record in self:
+            label_name = record.partner_id.bank_ids.bank_name if record.partner_id.bank_ids else False
+            label_number = record.partner_id.bank_ids.acc_number if record.partner_id.bank_ids else False
+            record.name = label_name + ' - ' + label_number if label_name and label_number else False
